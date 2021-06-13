d936df3d4f2cf4802ee73f192518fc168782027f
Ben <s3729184@student.rmit.edu.au>
2021-06-13 15:38:28 +1000

    final testing

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  browser = await launch({mongoURI: process.env.DB_URL || "mongodb://localhost:27017/tododb"})
+  mongoURI: process.env.DB_URL || "mongodb://localhost:27017/tododb"
 }
